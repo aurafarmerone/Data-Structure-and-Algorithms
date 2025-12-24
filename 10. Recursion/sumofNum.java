b96798ee25c713ab8e7381fcbf595dab1c4b82ac
aurafarmerone <aurafarmerone@gmail.com>
2025-12-24 13:46:51 +0530

    Update SumofNum

--- 10. Recursion/sumofNum.java
+++ 10. Recursion/sumofNum.java
@@ -2,21 +2,25 @@ public class sumofNum {
     public static void main(String[] args) {
         // RECURSION
         int n = 5;
-        System.out.print(" Sum till "+n+ "th term is : "+ sum(5));
-        
-        
-        
+        printSum(5);
+       
     }
 
-    public static int sum(int n){
+    public static void printSum(int n , int sum){
        // BASE CASE
         if(n==0){
-            return 0;
+            System.out.println("Sum = "+sum);
+            return;
         }
         
         //WORK + TRUST THE FUNCTION
-        return n + sum(n-1);
+        sum = sum + n ;
+        printSum(n-1,sum);
     }
 
+        // Overloaded method (acts like default sum = 0)
+    public static void printSum(int n) {
+        printSum(n, 0);
+    }
      
 }
